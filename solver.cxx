af29eb470bbb0a68faffd8a9943da1dd97b8d224
sathyakandada <sathyakandada@gmail.com>
2017-02-03 18:56:39 +0530

    Find fault solver

--- solver.cxx
+++ solver.cxx
@@ -1,11 +1,16 @@
 #include <iostream>
 #include "FEM1D.h"
 #include <vector>
+#include <ctime>
 
 int main() {
 
+	int start_clock = clock();
+
+	std::cout << "Execution started at: " << start_clock << std::endl;
+
 	// Spatial paramater
-	FEM1DDD sol; // 1 for 1D; 2 for 2D; 3 for 3D;
+	FEM1DDD sol; // FEM<Spatial Dimensions><Dir-Dir or Dir-Nue>
 
 	sol.obj_start = 0; // Object start point
 
@@ -21,24 +26,35 @@ int main() {
 
 	sol.bc2 = 0.001; // right end dirichlet
 
-	sol.conv_criteria = 0.00000001; // convergence criteria
+	sol.conv_criteria = 0.00001; // convergence criteria
 
-	sol.basisorder = 1; // Order of Basis Function
+	sol.basisorder = 2; // Order of Basis Function
 
 	char unimesh = 'y'; // uniform mesh = yes
 //	char unimesh = 'n'; // uniform mesh = no
 
 	if (unimesh == 'y') {
-		sol.numofele = 5; // user paramater for mesh
+		sol.numofele = 200; // user paramater for mesh
 		sol.Gen_Mesh();
 		sol.Dis_Mesh();
 		sol.Gen_kvec();
 		sol.Dis_kvec();
-		sol.Mat_ludc();
-//		sol.Dis_MtLU();
 		sol.Gen_forf();
 		sol.Gen_gvec();
-		sol.Sol_GaSi();
+//		sol.Sol_GaSi();
+//		sol.Sol_ludc();
+		sol.Sol_Guel();
+//		sol.Dis_MtLU();
+		sol.Dis_dvec();
 	}
 
+	int stop_clock = clock();
+
+	std::cout << "Execution finished at: " << stop_clock << std::endl;
+
+	double time_clock = (stop_clock - start_clock)/double(CLOCKS_PER_SEC)*1000;
+
+	std::cout << "Execution time: " << time_clock << "ms" << std::endl;
+
+return 0;
 }
